731fe6bbfc12a5281f9f3931eb25c41e5a565552
EmmanuelMercado <emmanuel_mercado_99@hotmail.com>
2023-03-15 16:52:20 -0600

    Tarea 1 Lista

--- FT-M1/01-IntroToCS/homework/homework.js
+++ FT-M1/01-IntroToCS/homework/homework.js
@@ -1,8 +1,35 @@
 'use strict';
 
-function BinarioADecimal(num) {}
-
-function DecimalABinario(num) {}
+function BinarioADecimal(num) {
+   let suma = 0;
+   let exponente= num.length-1;
+   for (let i=0; i<num.length; i++){
+      suma = suma + (num[i])*Math.pow(2,exponente);
+      exponente--;
+      
+   }
+   return (suma);
+   }
+function DecimalABinario(num) {
+   let binario = "";
+   let esDivisible = true;
+   while (esDivisible===true){
+      if (num%2==1){
+         binario = "1"+ binario; //100
+      }
+      else{
+         binario = "0"+ binario; //100
+      }
+      if (num>=2){
+         esDivisible=true;
+      }
+      else{
+         esDivisible=false;
+      }
+      num = parseInt(num/2);
+   }
+   return binario;
+}
 
 module.exports = {
    BinarioADecimal,
