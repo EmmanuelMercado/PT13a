d7ebc2d0500cce3949817d5a23a0be668227659d
EmmanuelMercado <emmanuel_mercado_99@hotmail.com>
2023-03-15 16:32:54 -0600

    Carpeta trabajo

---
+++ FT-M1/01-IntroToCS/homework/homework.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function BinarioADecimal(num) { // Recibe un String y retorna num
+  var resultado = 0 
+  for (){
+
+  }
+ }
+
+
+function DecimalABinario(num) {}
+
+module.exports = {
+   BinarioADecimal,
+   DecimalABinario,
+};
+// 8 en binario = 1000
